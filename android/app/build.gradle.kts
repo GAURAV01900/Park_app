96ba45c09d8954c1822388bf5ffcaf80f1311182
GAURAV01900 <shirodkargaurav5@gmail.com>
2025-10-26 16:39:35 +0530

    Pin NDK and minSdk to avoid manifest/NDK mismatch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,8 @@ dependencies {
 android {
     namespace = "com.example.park_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // Pin the highest NDK required by Firebase plugins to avoid NDK mismatch errors
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -37,9 +38,10 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.park_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+    // You can update the following values to match your application needs.
+    // For more information, see: https://flutter.dev/to/review-gradle-config.
+    // Firestore and some Firebase Android libraries require minSdk 23 or higher.
+    minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
